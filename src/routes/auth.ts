3cc95de0c71540fc447c1946111fef28ba66cb16
datoyacx <datoyacx@outlook.jp>
2021-09-28 05:57:59 +0700

    add detail api

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -31,21 +31,45 @@ router.post('/api/auth/register', async (req: Request, res: Response) => {
 
 router.post('/api/auth/login', async (req: Request, res: Response) => {
     const { username, password } = req.body;
-    const dateNow = getDate();
-    const getUser = await UserModel.findOne({ username: username });
-    const resultSend = {
-        'error': true
-    }
-    
-    if (getUser) {
-        const password_data = getUser['password'];
-        const isPasswordRight = await bcrypt.compare(password, password_data);
-        if (isPasswordRight) {
-            resultSend['error'] = false;
+    const responseStatus: ResponseStatus = {
+        'status': 400,
+        'error': {
+            'code': 1,
+            'message': 'something wrong'
         }
     }
+    const responseResult: any = {}
 
-    return res.status(200).send(resultSend);
+    if (username && password) {
+        const getUser = await UserModel.findOne({ username: username });
+        if (getUser) {
+            const password_data = getUser['password'];
+            const isPasswordRight = await bcrypt.compare(password, password_data);
+            if (isPasswordRight) {
+                responseStatus.status = 200;
+                responseResult.username = getUser.username;
+                responseResult.avatar = getUser.avatar;
+            } else {
+                responseStatus.status = 401;
+                responseStatus.error.code = 3;
+                responseStatus.error.message = 'invalid username or password, please try again';
+            }
+        } else {
+            responseStatus.status = 401;
+            responseStatus.error.code = 3;
+            responseStatus.error.message = 'invalid username or password, please try again';
+        }
+    } else {
+        responseStatus.status = 400;
+        responseStatus.error.code = 2;
+        responseStatus.error.message = 'username and password is required, please try again';
+    }
+
+    if (responseStatus.status != 200) {
+        responseResult.error = responseStatus.error;
+    }
+
+    return res.status(responseStatus.status).send(responseResult);
 });
 
 export { router as authRouter }

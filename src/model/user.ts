d64b86368e357635b6315754032b15dd97266fb2
datoyacx <datoyacx@outlook.jp>
2021-09-28 01:01:52 +0700

    add auto bcrypt password

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,5 +1,6 @@
 import { Model, Schema, model } from 'mongoose';
 import { User, QueryHelper } from '../interface/user';
+import bcrypt from 'bcrypt';
 
 const schema = new Schema<User, Model<User>, User>({
     username: { type: String, required: true },
@@ -8,5 +9,24 @@ const schema = new Schema<User, Model<User>, User>({
     created_at: { type: Number, required: true },
     updated_at: { type: Number, required: true }
 });
+
+schema.pre("save", function (next) {
+    const data = this
+    if (data.isModified("password") || data.isNew) {
+        bcrypt.genSalt(10, function (error, password_salt) {
+            if (error) {
+                return next(error);
+            } else {
+                bcrypt.hash(data.password, password_salt, function (error, password_hash) {
+                    if (error) { return next(error) }
+                    data.password = password_hash;
+                    next()
+                })
+            }
+        })
+    } else {
+        return next()
+    }
+})
 
 export const UserModel = model<User, Model<User, QueryHelper>>('user', schema, 'user');

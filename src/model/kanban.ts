e4d468e5832823825e5219e8206108580c7b1096
datoyacx <datoyacx@outlook.jp>
2021-09-27 22:13:52 +0700

    no more pluralize collection name

--- src/model/kanban.ts
+++ src/model/kanban.ts
@@ -8,4 +8,4 @@ const schema = new Schema<Kanban, Model<Kanban>, Kanban>({
     updated_at: { type: Number, required: true }
 });
 
-export const KanbanModel = model<Kanban, Model<Kanban, QueryHelper>>('list', schema)
+export const KanbanModel = model<Kanban, Model<Kanban, QueryHelper>>('list', schema, 'list');

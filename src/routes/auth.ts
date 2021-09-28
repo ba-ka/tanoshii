6f333d9a1f6b19dbac29ba77c67eaa327bb10201
datoyacx <datoyacx@outlook.jp>
2021-09-28 21:04:40 +0700

    add status code message to auth register

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -12,23 +12,47 @@ const router = express.Router();
 router.post('/api/auth/register', async (req: Request, res: Response) => {
     const { username, password } = req.body;
     const dateNow = getDate();
-    const getUser = await UserModel.findOne({ username: username });
+    const responseStatus: ResponseStatus = {
+        'status': 400,
+        'error': {
+            'code': 1,
+            'message': 'something wrong'
+        }
+    }
+    const responseResult: any = {}
 
-    if (!getUser) {
-        UserModel.create({
-            username: username,
-            password: password,
-            avatar: 'none',
-            updated_at: dateNow,
-            created_at: dateNow
-        }).then((result) => {
-            console.log("document inserted");
-        }).catch((error) => {
-            console.log(error.Message);
-        });
+    if (username && password) {
+        const getUser = await UserModel.findOne({ username: username });
+        if (!getUser) {
+            const createUser = await UserModel.create({
+                username: username,
+                password: password,
+                avatar: 'none',
+                updated_at: dateNow,
+                created_at: dateNow
+            });
+            if (createUser) {
+                responseStatus.status = 200;
+                responseResult.username = createUser.username;
+                responseResult.avatar = createUser.avatar;
+            }
+        } else {
+            responseStatus.status = 403;
+            responseStatus.error.code = 3;
+            responseStatus.error.message = 'username already used, please try again';
+        }
+    
+    } else {
+        responseStatus.status = 400;
+        responseStatus.error.code = 2;
+        responseStatus.error.message = 'username and password is required, please try again';
     }
 
-    return res.status(200).send('ok');
+    if (responseStatus.status != 200) {
+        responseResult.error = responseStatus.error;
+    }
+
+    return res.status(responseStatus.status).send(responseResult);
 });
 
 router.post('/api/auth/login', async (req: Request, res: Response) => {

2f64c571cbc5060454e4139426c5f9761156d400
datoyacx <datoyacx@outlook.jp>
2021-10-01 07:33:12 +0700

    add kanban board and task

--- src/interface/kanban/index.ts
+++ src/interface/kanban/index.ts
@@ -1,10 +1,13 @@
 import QueryHelper from './queryHelper';
+import { KanbanBoard, QueryHelper as KanbanBoardQueryHelper } from './board';
+import { KanbanTask, QueryHelper as KanbanTaskQueryHelper } from './task';
 
 interface Kanban {
     title: string;
     description: string;
+    board: string[];
     created_at: number;
     updated_at: number;
 }
 
-export { Kanban, QueryHelper }
+export { Kanban, QueryHelper, KanbanBoard, KanbanBoardQueryHelper, KanbanTask, KanbanTaskQueryHelper }

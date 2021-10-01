d6bc5dcec406f2156e46f23dee65212a0eda4bde
datoyacx <datoyacx@outlook.jp>
2021-10-01 08:07:09 +0700

    add kanban_id

--- src/interface/kanban/board/index.ts
+++ src/interface/kanban/board/index.ts
@@ -3,6 +3,7 @@ import QueryHelper from './queryHelper';
 interface KanbanBoard {
     title: string;
     description: string;
+    kanban_id: string;
     task: string[];
     created_at: number;
     updated_at: number;
